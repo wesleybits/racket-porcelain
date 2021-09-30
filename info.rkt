1906dd112298d9dce149779a2f8f4ab66023fd76
wesleybits <wesley.bitomski@gmail.com>
2021-09-29 22:12:19 -0400

    initial offerings

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define name "porcelain")
+(define version "1.0.0")
+(define deps '(racket-lib))
+(define pkg-description "Some trivial porcelains to make some Racket things prettier")
+(define pkg-authors '("wesley.bitomski@gmail.com"))
---
+++ process.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(provide (all-defined-out))
+
+(define-syntax-rule (allow-program name path)
+  (define (name . args)
+    (apply process* path (map ~a (flatten args)))))
+
+(define-syntax-rule (allow-command name)
+  (allow-program name (find-executable-path (~a (quote name)))))
+
+(define stdout first)
+(define stdin second)
+(define pid third)
+(define stderr fourth)
+
+(define (ctrl p sig)
+  ((fifth p) sig))
+
+(define (proc-wait p) (ctrl p 'wait))
+(define (proc-status p) (ctrl p 'status))
+(define (proc-exit-code p) (ctrl p 'exit-code))
+(define (proc-kill p) (ctrl p 'kill))
+(define (proc-interrupt p) (ctrl p 'interrupt))
+
+(define (read-output p #:port [port stdout] #:reader [reader port->string])
+  (reader (port p)))
+
+(define (trace-output p #:port [port stdout] #:tracer [trace displayln])
+  (for ([line (in-lines (port p))])
+    (trace line)))
+
+(define (tail p [out (current-output-port)])
+  (let loop:tail ()
+    (let ([stdout-line (sync/timeout 0.01 (read-line-evt (stdout p)))]
+          [stderr-line (sync/timeout 0.01 (read-line-evt (stderr p)))])
+      (when (and stdout-line (not (eof-object? stdout-line)))
+        (displayln stdout-line out))
+      (when (and stderr-line (not (eof-object? stderr-line)))
+        (display #\u001B out)
+        (display "[31m" out)
+        (display stderr-line out)
+        (display #\u001B out)
+        (display "[0m" out)
+        (newline out))
+      (unless (and (eof-object? stdout-line)
+                   (eof-object? stderr-line))
+        (loop:tail))))
+  p)
+
