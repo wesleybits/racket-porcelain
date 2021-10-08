38a0fcbb9855e177ec78e2c725dfd5d23c7ac96c
Wesley Bitomski <wesley@gradient.tech>
2021-10-08 15:59:26 -0400

    feat: added pretty-print json

--- json.rkt
+++ json.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (provide (all-defined-out))
+(require json)
 
 (define (jsexpr-walk expr . path)
   (match path
@@ -21,3 +22,47 @@
     [_                                            #:when (nor (list? expr) (hash? expr))
                                                   (error 'jsexpr-walk "Not a JSON structure type: ~w" expr)]))
 
+(define (pretty-print-json jsexpr [indent-width 4] [out (current-output-port)])
+  (define (print-indent width)
+    (display (make-string width #\space) out))
+
+  (define (print-json-object obj indent)
+    (let ([indent* (+ indent indent-width)])
+      (displayln "{" out)
+      (let loop:keys ([keys (hash-keys obj)])
+        (match keys
+          ['()                      (void)]
+          [(list key more-keys ...) (print-indent indent*)
+                                    (write (symbol->string key) out)
+                                    (display ": " out)
+                                    (print-json (hash-ref obj key) indent*)
+                                    (cond [(not (empty? more-keys)) (displayln "," out)
+                                                                    (loop:keys more-keys)]
+                                          [else                     (newline)])]))
+      (print-indent indent)
+      (display "}" out)))
+
+  (define (print-json-array arr indent)
+    (let ([indent* (+ indent indent-width)])
+      (displayln "[" out)
+      (let loop:elements ([elts arr])
+        (match elts
+          ['() (void)]
+          [(list elt rest-elts ...) (print-indent indent*)
+                                    (print-json elt indent*)
+                                    (cond [(not (empty? rest-elts)) (displayln "," out)
+                                                                    (loop:elements rest-elts)]
+                                          [else                     (newline)])]))
+      (print-indent indent)
+      (display "]" out)))
+
+  (define (print-json jsexpr indent)
+    (cond [(hash? jsexpr)           (print-json-object jsexpr indent)]
+          [(list? jsexpr)           (print-json-array jsexpr indent)]
+          [(number? jsexpr)         (display jsexpr out)]
+          [(boolean? jsexpr)        (display (if jsexpr "true" "false") out)]
+          [(string? jsexpr)         (write jsexpr out)]
+          [(eq? (json-null) jsexpr) (display "null" out)]
+          [else                     (error 'pretty-print-json/print-json "Not a json object: ~a" jsexpr)]))
+
+  (print-json jsexpr 0))
